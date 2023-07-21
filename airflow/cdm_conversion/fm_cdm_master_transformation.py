35727708779d1e195bec267c0bd4896f76588556
Dom <dom.silva@igt.com>
2023-07-21 09:09:28 -0400

    CDM File

--- airflow/cdm_conversion/fm_cdm_master_transformation.py
+++ airflow/cdm_conversion/fm_cdm_master_transformation.py
@@ -1,28 +1,18 @@
 """
 This is a workflow test.
 """
-
-from snowflake.snowpark.session import Session
-from snowflake.snowpark.functions import sproc, col, min, max, avg
-from snowflake.snowpark import functions as F
-from snowflake.snowpark.functions import concat, lit, col, lag, lead,lower,trim,upper
-from snowflake.snowpark.window import Window
-from snowflake.snowpark import types as T
-import datetime as dt
-import time
-from snowflake.snowpark.types import StringType
 from pathlib import Path
 import configparser
 import os
 
 # To import modules from another directory into the current directory
 import sys
+
 root_dir = Path(__file__).parent.parent.parent
 sys.path.insert(0, root_dir.__str__())
 
 
 def init_config():
-
     """
     This function does nothing.
 
@@ -36,11 +26,12 @@ def init_config():
     config = configparser.ConfigParser()
 
     parentFolderPath = Path(__file__).parent.parent.parent
-    config_file_path = os.path.join(parentFolderPath, 'common/utils', 'config.ini')
+    config_file_path = os.path.join(parentFolderPath, "common/utils", "config.ini")
 
     config.read(config_file_path)
 
     return config
+
 
 def hello_world():
     """
@@ -48,15 +39,15 @@ def hello_world():
 
     Args:
         None
-    
+
     Return:
         None
     """
 
     print("Hello")
 
-def add(x, y):
 
+def add(x, y):
     """
     Add two numbers together
 
@@ -71,7 +62,6 @@ def add(x, y):
 
 
 def sub(x, y):
-
     """
     Substract two numbers together
 
